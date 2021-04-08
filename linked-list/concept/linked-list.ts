3cb8754023f161bf0a4a20d2db209a40022e87d1
bhavesh.dewangan <bhavesh.dewangan@utopusinsights.com>
2021-04-08 21:19:10 +0530

    added few problem solution

--- linked-list/concept/linked-list.ts
+++ linked-list/concept/linked-list.ts
@@ -1,39 +1,39 @@
 import { NodeStructure } from './node-structure';
 export class LinkedList {
-    public head:NodeStructure = null; public tail:any; public value:any; public next:any; public prev:any; 
+    public head: NodeStructure = null; public tail: any; public value: any; public next: any; public prev: any;
     public prevNode = null;
-    public size:number;
+    public size: number;
     public callingCounter: number = 0;
-    constructor() {}
-    
+    constructor() { }
+
     addNode(element) {
         let node = new NodeStructure(element)
         let currentNode = null;
 
-        if(this.head == null){
+        if (this.head == null) {
             this.head = node;
         }
-        else{
+        else {
             currentNode = this.head;
-            while(currentNode.next){
+            while (currentNode.next) {
                 currentNode = currentNode.next
             }
             currentNode.next = node;
         }
     }
 
-    deleteNode(){
+    deleteNode() {
         let currentNode = null;
         let previousNode = null;
-        if(this.head == null){
+        if (this.head == null) {
             return true;
         }
-        else{
+        else {
             currentNode = this.head;
-            if(!currentNode.next)
+            if (!currentNode.next)
                 this.head = null;
-            else{
-                while(currentNode.next){
+            else {
+                while (currentNode.next) {
                     previousNode = currentNode;
                     currentNode = currentNode.next;
                 }
@@ -43,49 +43,49 @@ export class LinkedList {
         }
     }
 
-    deleteNodeAtPosition(position: number){
+    deleteNodeAtPosition(position: number) {
         let currentNode = null;
         let previousNode = null;
-        if(this.head == null){
+        if (this.head == null) {
             return true;
         }
-        else{
+        else {
             currentNode = this.head;
             let counter = 1;
-            if(counter==position){
+            if (counter == position) {
                 this.head = this.head.next
             }
-            do{
-                if(currentNode.next){
+            do {
+                if (currentNode.next) {
                     previousNode = currentNode
                     currentNode = currentNode.next;
                     counter++;
-                    if(counter == position){
+                    if (counter == position) {
                         previousNode.next = currentNode.next
                     }
                 }
             }
-            while(currentNode.next){ }
+            while (currentNode.next) { }
             return true;
         }
     }
 
-    addNodeAtPosition(element:any,position: number) {
+    addNodeAtPosition(element: any, position: number) {
         let node = new NodeStructure(element)
         let currentNode = null;
         let previousNode = null
-        
-        if(this.head == null){
+
+        if (this.head == null) {
             return 0;
         }
-        else{
+        else {
             let counter = 1;
             currentNode = this.head;
-            while(currentNode.next){
+            while (currentNode.next) {
                 previousNode = currentNode;
                 currentNode = currentNode.next;
                 counter++;
-                if(counter == position){
+                if (counter == position) {
                     previousNode.next = node;
                     node.next = currentNode;
                 }
@@ -96,8 +96,8 @@ export class LinkedList {
     displayAllNode(): Number {
         let counter = 0; let nodeChain = ''
         let node = this.head;
-        
-        while(node){
+
+        while (node) {
             nodeChain = nodeChain + node.element + '-->';
             counter++;
             node = node.next;
@@ -107,22 +107,12 @@ export class LinkedList {
     }
 
     //Reverse a Linked List in groups of given size | Set 1
-    reverseNodeOfGroup(size:number){
+    reverseNodeOfGroup(size: number) {
         let currentNode = this.head;
         let prevNode = null;
         let nextNode = null;
         let counter = 0;
-
-        // Simple-Reverse
-        // while(currentNode ){
-        //     nextNode = currentNode.next;
-        //     currentNode.next = prevNode;
-        //     prevNode = currentNode;
-        //     currentNode = nextNode;
-        //     counter++;
-        //}
-
-        while(currentNode.next && counter<size){
+        while (currentNode.next && counter < size) {
             nextNode = currentNode.next;
             currentNode.next = prevNode;
             prevNode = currentNode;
@@ -133,35 +123,107 @@ export class LinkedList {
         this.head = prevNode;
     }
 
-    reverseWithRecursive(currentNode: NodeStructure){
-        console.log('the calling counter , value : ', ++this.callingCounter , currentNode.element);
-        let currNode = currentNode;
-        if(currNode.next ==  null)
-        {
-            this.head = currNode;
+    reverseWithRecursive(node: NodeStructure) {
+        let currentNode = node;
+        if (currentNode.next == null) {
+            this.head = currentNode;
             return 0;
         }
-        this.reverseWithRecursive(currNode.next);
-        console.log('currNode ---> \n', currNode);
-        let nextNode = currNode.next;
-        // console.log('next node value', nextNode);
-        nextNode.next = currNode;
-        // console.log('next node  next value ', nextNode.next);
-        console.log('current node ----> \n', nextNode );
-        console.log('Next Node ----> \n', nextNode.next);
+        this.reverseWithRecursive(currentNode.next);
+        let nextNode = currentNode.next;
+        nextNode.next = currentNode;
         currentNode.next = null;
-        
+    }
+
+    simpleReverse(head) {
+        let currentNode: NodeStructure = JSON.parse(JSON.stringify(head));;
+        let prevNode: NodeStructure = null;
+        let nextNode: NodeStructure = null;
+        if (head == null || head.next == null) {
+            return head;
+        } else {
+            while (currentNode) {
+                nextNode = currentNode.next
+                currentNode.next = prevNode
+                prevNode = currentNode;
+                currentNode = nextNode;
+            }
+            this.head = prevNode
+            head = prevNode;
+        }
+        return head
+    }
+
+    // Reverse a sublist of linked list
+    reverseSublistOfLinkedList(startIndex: number, endIndex: number, head: NodeStructure) {
+        let headCopy = head;
+        if (!head) {
+            return 0;
+        }
+        let totleElement = 0;
+        while (headCopy) {
+            headCopy = headCopy.next;
+            totleElement++;
+        }
+        if (totleElement >= endIndex) {
+            let nextNode = null, prevNode = null, currentNode = head;
+            let startIndexNode = null
+            let start = 1
+            while (currentNode && start <= endIndex) {
+                if (start >= startIndex && start <= endIndex) {
+                    nextNode = currentNode.next;
+                    currentNode.next = prevNode;
+                    prevNode = currentNode;
+                    currentNode = nextNode;
+                } else {
+                    startIndexNode = currentNode;
+                    currentNode = currentNode.next
+                }
+                start++;
+            }
+            console.log("startIndexNode ", startIndexNode);
+            console.log("CURRENT", currentNode);
+            console.log("PREVIOUS", prevNode);
+            console.log("NEXT", nextNode);
+
+            if (startIndexNode) {
+                startIndexNode.next = prevNode;
+            } else {
+                startIndexNode = head = prevNode;
+            }
+
+            if (currentNode) {
+                while (startIndexNode.next) {
+                    startIndexNode = startIndexNode.next;
+                }
+                startIndexNode.next = currentNode;
+            }
+
+        }
+    }
+
+    checkPalindrom(node) {
+        let nodeCopy = this.simpleReverse(JSON.parse(JSON.stringify(node)));
+        while (node && nodeCopy) {
+            if (node.element !== nodeCopy.element) {
+                console.log("NOT PALINDROM");
+                return false;
+            }
+            node = node.next;
+            nodeCopy = nodeCopy.next;
+        }
+        console.log("ITS A PALINDROM");
+        return true;
     }
 }
 
-var toDo = new LinkedList();
-// var toDoNext = new LinkedList()
-// var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-// for(var i=0; i<3; i++){
+// var toDo = new LinkedList();
+// for (var i = 0; i < 3; i++) {
 //     toDo.addNode(i);
-//     toDoNext.addNode(i);
 // }
-
+// toDo.simpleReverse(toDo.head)
+// toDo.reverseWithRecursive(toDo.head)
+// toDo.displayAllNode();
 
 // delete node
 // toDo.deleteNode();
@@ -174,11 +236,12 @@ var toDo = new LinkedList();
 
 // reverse node 
 // toDo.reverseNodeOfGroup(5);
-// toDoNext.reverseNodeOfGroup(8);
 
 // Reverse Recursive
 // toDo.reverseWithRecursive(toDo.head);
 // toDo.displayAllNode();
+
+
 
 
 
